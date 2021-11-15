f9567f4d7d228f8cf98d3cfe72eadcc95e0966f1
tahjaythompson <thompsontahjay34@gmail.com>
2021-11-15 10:59:56 -0500

    Added new design to the app

--- pages/index.js
+++ pages/index.js
@@ -11,11 +11,11 @@ export default function Home() {
         <>
         <Head>
         <title>Affirmation City</title>
-          <meta name="Description" content="" />
+          <meta name="Description" content="daily affirmations are clear, positive statements that describe your goals as already achieved. Get Inspired now! " />
           <meta property="og:title" content="Affirmation City"  />
           <meta property="og:type" content="website" />
           <meta property="og:url" content="https://affirmation.tahjaythompson.com/" />
-          <meta property="og:description" content="" />
+          <meta property="og:description" content="daily affirmations are clear, positive statements that describe your goals as already achieved. Get Inspired now!" />
           <meta property="og:determiner" content="the" />
           <meta property="og:locale" content="en_US" />
           <meta property="og:locale:alternate" content="fr_FR" />
@@ -31,12 +31,12 @@ export default function Home() {
           <meta property="og:image:width" content="1080"/>
           <meta name="twitter:card" content="summary_large_image"/>
           <meta name="twitter:title" content="Affirmation City"/>
-          <meta name="twitter:description"  content=""/>
+          <meta name="twitter:description"  content="daily affirmations are clear, positive statements that describe your goals as already achieved. Get Inspired now!"/>
           <meta name="twitter:site" content="https://affirmation.tahjaythompson.com/"/>
         </Head>
         <div className = { styles.section} >
             <h1 > Get Daily Affirmations </h1>  
-            <h2 > Affirmation City </h2> 
+            <h2 className={styles.top}> Affirmation City </h2> 
             <img src="https://img.icons8.com/clouds/100/000000/tribal-symbols.png"/>
 
          </div>
@@ -47,6 +47,25 @@ export default function Home() {
                 affirmations[Math.floor(Math.random() * affirmations.length)]
                 } 
             </div>
+        </div>
+
+        <div className={styles.info}>
+        <h2>        What Are Daily Affirmations?    
+</h2>
+
+        <p>Image result for What Are Daily Affirmations?
+Simply put, daily affirmations are clear, positive statements that describe your goals as already achieved. </p>
+        </div>
+
+        <div className={styles.info}>
+            <h2>What are daily affirmations used for?</h2>
+        <p>Image result for What Are Daily Affirmations?
+Simply put, daily affirmations are clear, positive statements that describe your goals as already achieved. </p>
+        </div>
+
+        <div className={styles.info}>
+            <h2>What is a positive affirmation?</h2>
+        <p>Affirmations are positive statements that can help you to challenge and overcome self-sabotaging and negative thoughts. When you repeat them often, and believe in them, you can start to make positive changes. </p>
         </div>
 
         </>
