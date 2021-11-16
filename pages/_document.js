2ff21f59689f80cf4fab81c8eb465986d3d38f69
tahjaythompson <thompsontahjay34@gmail.com>
2021-11-16 14:10:40 -0500

    made changes

--- pages/_document.js
+++ pages/_document.js
@@ -11,7 +11,6 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com"/>
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
           <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;900&display=swap" rel="stylesheet"/>
-          <script src="dist/clipboard.min.js"></script>
 
                     
         </Head>     
--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
       }
     const [state,setState] = useState("Copy");
     const [change,setChange] = useState(false);
-    setInterval(function(){setChange(!change)}, 300000);
+    // setInterval(function(){setChange(!change)}, 300000);
 
     return(
         <>
@@ -52,8 +52,11 @@ export default function Home() {
          </div>
 
 
-        <div className={styles.container} id='affirmation_field'> 
-            <div className={styles.affirm} >
+        <div className={styles.container} id='affirmation_field' onClick={()=>{copyTextToClipboard(document.getElementById("affirmation_field").innerText)
+                    setState("Copied");
+                    
+                }}> 
+            <div className={styles.affirm}  >
                 {
                 change == false? affirmations[Math.floor(Math.random() * affirmations.length)]:affirmations[Math.floor(Math.random() * affirmations.length)]
                 } 
@@ -76,12 +79,12 @@ export default function Home() {
         }}>
            
             {state=="Copy"? 
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clipboard" viewBox="0 0 16 16">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"  viewBox="0 0 16 16">
                 <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"/>
                 <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"/>
                 </svg> 
             :
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-all" viewBox="0 0 16 16">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"  viewBox="0 0 16 16">
                 <path d="M8.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L2.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093L8.95 4.992a.252.252 0 0 1 .02-.022zm-.92 5.14.92.92a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 1 0-1.091-1.028L9.477 9.417l-.485-.486-.943 1.179z"/>
                 </svg> 
           }{state}
