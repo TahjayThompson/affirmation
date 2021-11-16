245dc1b22101eec90761809831d0bb39695ada66
tahjaythompson <thompsontahjay34@gmail.com>
2021-11-16 14:01:20 -0500

    Added btn to copy

--- pages/_document.js
+++ pages/_document.js
@@ -11,6 +11,7 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com"/>
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
           <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;900&display=swap" rel="stylesheet"/>
+          <script src="dist/clipboard.min.js"></script>
 
                     
         </Head>     
